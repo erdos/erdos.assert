0a37a18268d5b59c36fc0f8f736e20ed883b9f2f
Janos Erdos <erdosj@gmail.com>
2018-09-29 17:48:14 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject erdos.assert "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://github.com/erdos.assert"
+  :description "smart assertions library for Clojure"
+  :url "http://github.com/erdos/erdos.assert"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
