cf8a51448ea9f5a72534ceb6c7621533ed5f6840
Janos Erdos <janos@transferwise.com>
2021-02-15 17:55:25 +0100

    chore: test cases

--- test/erdos/assert_test.clj
+++ test/erdos/assert_test.clj
@@ -101,21 +101,25 @@
 (deftest test-is
   (ea/is (= 2 (inc 1))))
 
+(deftest test-are
+  (ea/are [x y] (= (inc x) y)
+     1 2
+     2 3
+     4 5))
 
-                                        ; (ea/examine (* (+ 19 17) (- 19 17)))
+(comment ;; run these test cases manually
 
-;
+  (examine (* (+ 19 17) (- 19 17)))
 
-; (ea
+  (examine (= (:a {:a (str "asd")}) (or (+ 1 2) (+ 3 4) (+ 5 6)) (+ 2 (- 4 3 (* 2 3 4)))))
 
-                                        ;(examine (= (:a {:a (str "asd")}) (or (+ 1 2) (+ 3 4) (+ 5 6)) (+ 2 (- 4 3 (* 2 3 4)))))
+  (examine (let [a (+ 1 2) b (* 3 4)] (* (+ a b) (- a b))))
 
-                                        ;(examine (let [a (+ 1 2) b (* 3 4)] (* (+ a b) (- a b))))
+  (examine (map (fn [a] (+ a 1)) [1 2 3 4]))
 
-                                        ;(examine (map (fn [a] (+ a 1)) [1 2 3 4]))
+  (examine (doall (for [i [1 2 3 4]] (inc i))))
 
-                                        ;(examine (doall (for [i [1 2 3 4]] (inc i))))
+  (examine (doseq [i [1 2 3 4]] (print (* i i)) (print (* 2 i))))
 
-                                        ;(examine (doseq [i [1 2 3 4]] (print (* i i)) (print (* 2 i))))
-
-                                        ; (macroexpand-code '(doseq [i [1 2 3]] XXXX YYYY))
+  (-macroexpand-code '(doseq [i [1 2 3]] XXXX YYYY))
+)
